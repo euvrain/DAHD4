ddf987ee45a24ddc663461af812c208d68aa66a5
euvrain <taylormcdoc@gmail.com>
2026-06-26 08:56:22 -0400

    style: format code for improved readability and consistency

--- DAHM4_ex.m
+++ DAHM4_ex.m
@@ -1,5 +1,5 @@
 
-            function [EVP]=DAHM4_ex(vv,W,NF,NT)
+function [EVP]=DAHM4_ex(vv,W,NF,NT)
 %  this function computes EVP - space-time DAHMs obtained 
 %  by an inverse Fourier transform of Hermitian DAHD eigenvectors vv
 %  W -  embedding window
@@ -13,31 +13,31 @@
 %  Chaos: 36, doi: 10.1063/5.0295634
 %  Written by Dmitri Kondrashov.   Version date 6/22/26
 %  Please send comments and suggestions to dkondras@atmos.ucla.edu           
- 
-            D=size(vv,1);
 
-            EVP=zeros((2*W-1)*D,NT);
+    D=size(vv,1);
 
-            fftv=zeros(2*W-1,D,NT);
-           if NF > 1
-           for i=1:NT/2
-           i2=2*i-1;
-           cvv=sqrt(-1)*vv(:,i);
-           fftv(NF,:,i2)=sqrt(W-0.5)*vv(:,i);
-           fftv(end-NF+2,:,i2)=sqrt(W-0.5)*conj(vv(:,i));
-           fftv(NF,:,i2+1)=sqrt(W-0.5)*cvv;
-           fftv(end-NF+2,:,i2+1)=sqrt(W-0.5)*conj(cvv);
-           end
-           else
-           for i=1:NT/2
-           cvv=sqrt(-1)*vv(:,i);
-           fftv(NF,:,i)=sqrt(2*W-1)*vv(:,i);
-           end
-           end
-           Evv=ifft(fftv,[],1);
-           EVP=reshape(Evv,size(Evv,1)*size(Evv,2),size(Evv,3));
+    EVP=zeros((2*W-1)*D,NT);
+
+    fftv=zeros(2*W-1,D,NT);
+if NF > 1
+for i=1:NT/2
+i2=2*i-1;
+cvv=sqrt(-1)*vv(:,i);
+fftv(NF,:,i2)=sqrt(W-0.5)*vv(:,i);
+fftv(end-NF+2,:,i2)=sqrt(W-0.5)*conj(vv(:,i));
+fftv(NF,:,i2+1)=sqrt(W-0.5)*cvv;
+fftv(end-NF+2,:,i2+1)=sqrt(W-0.5)*conj(cvv);
+end
+else
+for i=1:NT/2
+cvv=sqrt(-1)*vv(:,i);
+fftv(NF,:,i)=sqrt(2*W-1)*vv(:,i);
+end
+end
+Evv=ifft(fftv,[],1);
+EVP=reshape(Evv,size(Evv,1)*size(Evv,2),size(Evv,3));
 
 
 
-           return EVP %fix: empty return statement
+return EVP %fix: empty return statement
 
--- dahd4example.py
+++ dahd4example.py
@@ -1,53 +1,116 @@
 import numpy as np
-from center import center
+import matplotlib.pyplot as plt
 from time import time
-from matplotlib import pyplot as plt
-#lear all
-#general_data
-x='hold'
-fpcs=center(x)
-W=(fpcs.shape[0]+1)/2; #maximum window length
-W=65
-WW=2*W-1
-D=fpcs.shape[1]
-NP=D #maximum is D
-NFE=W # maximum is W
+from center import center
+from dahc import dahc
+from hrc import hrc
+from DAHD4freq_part_weight import DAHD4freq_part_weight
+from DAHM4_ex import DAHM4_ex
+from generate_data import x, xref1, xref2, xref3, xref4, N
 
+# Center data
+fpcs, _ = center(x)
 
-wt = 'hann'
-wt='blackman'
-wt='hamming'
-wt='bartlett'
-#wt='none'
+W = int((fpcs.shape[0] + 1) / 2)   # maximum window length
+W = 65
+WW = 2 * W - 1
+D = fpcs.shape[1]
+NP = D      # maximum is D
+NFE = W     # maximum is W
+
+# wt = 'hann'
+# wt = 'blackman'
+# wt = 'hanning'
+# wt = 'hamming'
+# wt = 'bartlett'
+wt = 'hamming'
+# wt = 'none'
+
 start = time()
+fE2, VP, FEP = DAHD4freq_part_weight(fpcs, W, NFE, NP, wt)
+print(f"Elapsed time: {time() - start:.4f} seconds")
 
-
-fE2,VP,FEP=DAHD4freq_part_weight(fpcs,W,NFE,NP,wt)
-elapsed = time()-start
-#%% DAHD spectrum
-NP=1
+# DAHD spectrum
+NP = 1
 plt.figure()
 plt.rcParams.update({'font.size': 16})
-plt.semilogy(fE2[1:],abs(VP[:,1:]),'ro',markersize=4,markerfacecolor='r')
-#hold on
-plt.semilogy(fE2[17],abs(VP[:NP,17]),'bo',markersize=8,markerfacecolor='b')
-plt.semilogy(fE2[26],abs(VP[:NP,26]),'co',markersize=8,markerfacecolor='c')
-plt.semilogy(fE2[46],abs(VP[:NP,46]),'go',markersize=8,markerfacecolor='g')
-plt.semilogy(fE2(57),abs(VP[:NP,56]),'ko',markersize=8,markerfacecolor='k')
-plt.ylabel('\lambda')
-#xlim([0 0.5]);
-#ylim([1.e-2 Inf]);
-#xlabel('Freq')
-#set(gca,'FontSize',16);
-#%% compute DAHMs
-# EP=zeros((2*W-1)*D,2*NP,NFE);%% 
-# X=reshape(fpcs,size(fpcs,1)*D,1);
-# tic
-#for ifff in range(NFE):
+plt.semilogy(fE2[1:], np.abs(VP[:, 1:]), 'ro', markersize=4, markerfacecolor='r')
+plt.semilogy(fE2[17], np.abs(VP[:NP, 17]), 'bo', markersize=8, markerfacecolor='b')
+plt.semilogy(fE2[26], np.abs(VP[:NP, 26]), 'co', markersize=8, markerfacecolor='c')
+plt.semilogy(fE2[46], np.abs(VP[:NP, 46]), 'go', markersize=8, markerfacecolor='g')
+plt.semilogy(fE2[56], np.abs(VP[:NP, 56]), 'ko', markersize=8, markerfacecolor='k')
+plt.ylabel(r'$\lambda$')
+plt.xlim([0, 0.5])
+plt.ylim([1e-2, np.inf])
+plt.xlabel('Freq')
+plt.show()
 
-# ER=DAHM4_ex(FEP(:,:,iff),W,iff,2*NP);
-# ERR=reshape(ER,(2*W-1)*D,2*NP);
-# EP(:,:,iff)=ERR;
-# end
+# Compute DAHMs
+EP = np.zeros(((2 * W - 1) * D, 2 * NP, NFE))
+start = time()
+for iff in range(NFE):
+    ER = DAHM4_ex(FEP[:, :, iff], W, iff, 2 * NP)
+    ERR = np.reshape(ER, ((2 * W - 1) * D, 2 * NP))
+    EP[:, :, iff] = ERR
+print(f"Elapsed time: {time() - start:.4f} seconds")
 
+tt = ['(a) f_1', '(b) f_2', '(c) f_3', '(d) f_4']
+ff = [17, 26, 46, 56]   # 0-indexed (MATLAB: 18, 27, 47, 57)
 
+fig = plt.figure(figsize=(20 / 2.54, 40 / 2.54), dpi=100)
+plt.rcParams.update({
+    'axes.labelweight': 'bold',
+    'axes.titleweight': 'bold',
+    'axes.labelsize': 10,
+    'axes.titlesize': 10,
+    'xtick.labelsize': 16,
+    'ytick.labelsize': 16,
+})
+
+kk = 0
+for pos in range(4):
+    for K in range(2):
+        kk += 1
+        plt.subplot(4, 2, kk)
+        cf = plt.contourf(
+            np.reshape(EP[:, K, ff[pos]], (WW, 6)).T,
+            20, cmap='jet'
+        )
+        plt.colorbar(cf)
+        if K == 0:
+            plt.title(tt[pos])
+        plt.xlabel('Time')
+        plt.ylabel('Space')
+
+plt.tight_layout()
+plt.show()
+
+# Reconstruction
+# Indices of frequencies of the harmonic modes for maximum window length
+ifff = [17, 26, 46, 56]   # 0-indexed (MATLAB: 18, 27, 47, 57)
+NE = fpcs.shape[0]
+PCF = np.zeros((NE, D, len(ifff)))
+AF = np.zeros((N - (2 * W - 1) + 1, 2, len(ifff)))
+NPP = 1     # reconstruct only with modes at spectral peak
+
+for i in range(len(ifff)):
+    iff = ifff[i]
+    tmp = np.squeeze(EP[:, :2 * NPP, iff])
+    A = dahc(fpcs, tmp)
+    AF[:, :, i] = A
+    PCF[:, :, i] = hrc(A, tmp, fpcs.shape[1], np.arange(2 * NPP))
+
+R1 = np.squeeze(PCF[:, :, 0])
+R2 = np.squeeze(PCF[:, :, 1])
+R3 = np.squeeze(PCF[:, :, 2])
+R4 = np.squeeze(PCF[:, :, 3])
+
+# Normalized RMSE of reconstruction
+print('Normalized RMSE of reconstruction:')
+print(np.sum(np.sum((xref1 - R1) ** 2, axis=1)) / np.sum(np.sum(xref1 ** 2, axis=1)))
+print(np.sum(np.sum((xref2 - R2) ** 2, axis=1)) / np.sum(np.sum(xref2 ** 2, axis=1)))
+print(np.sum(np.sum((xref3 - R3) ** 2, axis=1)) / np.sum(np.sum(xref3 ** 2, axis=1)))
+print(np.sum(np.sum((xref4 - R4) ** 2, axis=1)) / np.sum(np.sum(xref4 ** 2, axis=1)))
+
+# Plot reconstructed harmonic components (see plothrcmodes.py)
+exec(open('plothrcmodes.py').read())
